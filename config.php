38af2cfc4418709515f287d3c7efd9950f3474c8
mem03748 <77647155+mem03748@users.noreply.github.com>
2021-04-08 19:24:08 -0600

    Update config.php

--- config.php
+++ config.php
@@ -35,6 +35,39 @@ function pdo_connect_mysql() {
 
 
 
+function redirect($file, $msg, $color)
+{
+  if ($msg == 'right' || $msg == 'wrong') {
+    //make sure that answer is either right or wrong
+    header("Location: " . $file . "?answer=$msg");
+  } else {
+    echo 'there was an error.';
+  }
+}
+
+function message()
+{
+
+
+  if (strpos($_SERVER['REQUEST_URI'], "right") == true) {
+?>
+    <div class="notification is-success">
+      <h2 class="title is-2">Success!</h2>
+    </div>
+  <?php
+  }
+
+  if (strpos($_SERVER['REQUEST_URI'], "wrong") == true) {
+  ?>
+    <div class="notification is-danger">
+      <h2 class="title is-2">There was an error.</h2>
+    </div>
+<?php
+  }
+}
+
+
+
 function template_header($title = "Page title")
 {
 echo <<<EOT
