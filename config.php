ba6d5f9c09cf30f822a43676e01c431da5ad32e0
mem03748 <77647155+mem03748@users.noreply.github.com>
2021-04-08 20:22:46 -0600

    nav bar updated

--- config.php
+++ config.php
@@ -117,17 +117,21 @@ echo <<<EOT
           <div class="navbar-end">
             <div class="navbar-item">
               <div class="buttons">
-                <a href="profile.php" class="button">
+                <a href="index.php" class="button">
                   <span class="icon"><i class="fas fa-user"></i></span>
-                  <span>Profile</span>
+                  <span>Home</span>
                 </a>
-                <a href="#" class="button">
+                <a href="contact.php" class="button">
                   <span class="icon"><i class="fas fa-address-book"></i></span>
-                  <span>Button</span>
+                  <span>Contact Us</span>
+                </a>
+                <a href="login.php" class="button">
+                  <span class="icon"><i class="fas fa-sign-out-alt"></i></span>
+                  <span>Login</span>
                 </a>
                 <a href="logout.php" class="button">
                   <span class="icon"><i class="fas fa-sign-out-alt"></i></span>
-                  <span>Sign Out</span>
+                  <span>Logout</span>
                 </a>
               </div>
             </div>
@@ -136,7 +140,6 @@ echo <<<EOT
       </div>
     </nav>
     <!-- END NAV -->
-
     <!-- START MAIN -->
     <section class="section">
         <div class="container">
@@ -149,7 +152,6 @@ echo <<<EOT
         </div>
     </section>
     <!-- END MAIN-->
-
     <!-- START FOOTER -->
     <footer class="footer">
         <div class="container">
